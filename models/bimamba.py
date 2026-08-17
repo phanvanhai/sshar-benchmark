fc29d4c5165d695f0bd8bef7972c70c394b47e5f
phanvanhai <hai.phanv.hust@gmail.com>
2026-08-17 18:40:41 +0700

    change mamba config

--- models/bimamba.py
+++ models/bimamba.py
@@ -28,7 +28,7 @@ def get_bimamba_config(dataset, input_shape, num_classes):
             "in_channels": input_shape[0],
             "out_channels": input_shape[0],
             "kernel_size": 5,
-            "groups": 6,
+            "groups": 3,
         }
     elif dataset == "sshar_asus":
         return {
@@ -39,7 +39,7 @@ def get_bimamba_config(dataset, input_shape, num_classes):
             "in_channels": input_shape[0],
             "out_channels": input_shape[0],
             "kernel_size": 5,
-            "groups": 24,
+            "groups": 9,
         }
     elif dataset == "xrf55":
         return {
